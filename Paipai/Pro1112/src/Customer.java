d9f0dee10fa1f3abdbb686881f61f03e4bdcf7fa
luoyuan <luoyuan.myp@alibaba-inc.com>
2020-11-12 20:58:22 +0800

    1112 作业批注

--- Paipai/Pro1112/src/Customer.java
+++ Paipai/Pro1112/src/Customer.java
@@ -1,14 +1,34 @@
-import java.awt.Menu;
+//Menu 是接口，不是被 赋值 =，只能被实现 implement
+public class Customer implements Menu {
 
-public class Customer {
-    Menu m;
-    Customer(Menu m){
-        this.m=m;
+//    Customer(Menu m){
+//        this.m=m;
+//    }
+
+//    public void cook(){
+//      m.fish();
+//    }
+
+    Customer(){
+        // 写你的构造方法
+        System.out.println("customer's constructor be called");
+
+        this.fish();
     }
 
-    public void cook(){
-      m.fish();
+    @Override
+    public void chicken() {
 
+    }
 
+    @Override
+    public void fish() {
+        System.out.println("customer's fish-function be clicked ");
+
+    }
+
+    public void cook (){
+        System.out.println("customer's cook-function be clicked ，我做饭啦");
+        this.fish();
     }
 }
