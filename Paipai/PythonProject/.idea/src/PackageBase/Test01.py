3371173ada946d833c5320be944de98e0cabd382
EmmaLiuPie <liupai0303@163.com>
2020-12-25 11:41:09 +0800

    提交python

---
+++ Paipai/PythonProject/.idea/src/PackageBase/Test01.py
@@ -0,0 +1,7 @@
+def doSome(name):
+    print("welcome "+name)
+doSome("paipai")
+
+s="abcedfg"
+print(s[1:2])
+print(s[-5:-4])
--- Paipai/TestNgLearn1/src/main/java/cn/lp/Test01.java
+++
@@ -1,7 +0,0 @@
-package cn.lp;
-
-public class Test01 {
-    void sum(int a,int b){
-        int result = a+b;
-    }
-}
